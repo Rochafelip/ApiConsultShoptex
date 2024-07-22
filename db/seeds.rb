8a49b27fb588cc048b70ed211a99ba51b2570145
Felipe Rocha <rocha.felipe98@gmail.com>
2024-07-22 18:20:22 -0300

    Adicionando a Url das imagens e colocando mais produtos.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-Produto.create(name: "Placa Mãe ASUS Prime B450M-A", price: 450.00, photo: "https://example.com/motherboard.jpg", is_promotion: true)
-Produto.create(name: "Processador AMD Ryzen 5 3600", price: 1200.00, photo: "https://example.com/ryzen5.jpg", is_promotion: false)
-Produto.create(name: "Memória RAM Kingston 8GB DDR4", price: 300.00, photo: "https://example.com/ram.jpg", is_promotion: true)
-Produto.create(name: "SSD Samsung 860 EVO 500GB", price: 600.00, photo: "https://example.com/ssd.jpg", is_promotion: false)
-Produto.create(name: "Fonte Corsair 550W", price: 350.00, photo: "https://example.com/psu.jpg", is_promotion: true)
-Produto.create(name: "Gabinete Cooler Master MasterBox Q300L", price: 250.00, photo: "https://example.com/case.jpg", is_promotion: false)
-Produto.create(name: "Mouse Logitech MX Master 3", price: 450.00, photo: "https://example.com/mouse.jpg", is_promotion: true)
-Produto.create(name: "Teclado Mecânico Corsair K95 RGB", price: 750.00, photo: "https://example.com/keyboard.jpg", is_promotion: false)
+Produto.create(name: "Placa Mãe ASUS Prime B450M-A", price: 350.00, photo: "https://www.asus.com/media/global/gallery/ImMlb2x6hfHbMQrR_setting_xxx_0_90_end_800.png", is_promotion: true)
+
+Produto.create(name: "Processador AMD Ryzen 5 3600", price: 1200.00, photo: "https://images.kabum.com.br/produtos/fotos/102438/processador-amd-ryzen-5-3600-cache-32mb-3-6ghz-4-2ghz-max-turbo-am4-100-100000031box_processador-amd-ryzen-5-3600-cache-32mb-3-6ghz-4-2ghz-max-turbo-am4-100-100000031box_1562606710_g.jpg", is_promotion: false)
+
+Produto.create(name: "Memória RAM Xpg 16GB DDR4", price: 300.00, photo: "https://static.conecta.inf.br/public/conectainformatica/imagens/produtos/memoria-ddr4-16gb-3200mhz-xpg-gammix-d35-preta-655b58652e970.png", is_promotion: true)
+
+Produto.create(name: "SSD LEXAR 512 GB", price: 600.00, photo: "https://images.kabum.com.br/produtos/fotos/sync_mirakl/461453/SSD-Lexar-Nm620-M-2-2280-Pcie-512GB-Lnm620x512g-rnnnu_1718924617_g.jpg", is_promotion: false)
+
+Produto.create(name: "Fonte Corsair 550W", price: 350.00, photo: "https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/c/p/cp-90201774_1.jpg", is_promotion: true)
+
+Produto.create(name: "Gabinete Cooler Master MasterBox Q300L", price: 350.00, photo: "https://m.media-amazon.com/images/I/81I9Ef0fOIL._AC_UF894,1000_QL80_.jpg", is_promotion: false)
+
+Produto.create(name: "Mouse Logitech G203", price: 87.00, photo: "https://images.kabum.com.br/produtos/fotos/112948/mouse-gamer-logitech-g203-rgb-lightsync-6-botoes-8000-dpi-preto-910-005793_1612880277_gg.jpg", is_promotion: true)
+
+Produto.create(name: "Teclado Mecânico Logitech g915", price:  1249.00, photo: "https://images.kabum.com.br/produtos/fotos/sync_mirakl/173873/Teclado-Sem-Fio-Mec-nico-Gamer-Logitech-G915-Lightspeed-Bluetooth-Design-Ultrafino-RGB-Switch-Gl-Tactile-Marrom-US_1719528018_g.jpg", is_promotion: false)
+
+Produto.create(name: "Placa de Vídeo NVIDIA GeForce RTX 3060 FE", price: 2000.00, photo: "https://images.nvidia.com/aem-dam/Solutions/geforce/ampere/rtx-3060-ti/geforce-rtx-3060-ti-product-gallery-full-screen-3840-1-bl.jpg", is_promotion: true)
+
+Produto.create(name: "Monitor LG UltraGear 27GL850", price: 2500.00, photo: "https://m.media-amazon.com/images/I/7166tFSuGBL._AC_UF894,1000_QL80_.jpg", is_promotion: false)
